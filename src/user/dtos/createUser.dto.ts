959e2431d02705c464a49a4dc45b1159188790d6
Jefferson Barreto <jeffinho_fb@hotmail.com>
2023-09-22 20:14:48 -0300

    feat: insert UserEntity

--- src/user/dtos/createUser.dto.ts
+++ src/user/dtos/createUser.dto.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 export interface CreateUserDto {
   name: string;
   email: string;
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { UserEntity } from './interfaces/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserService } from './user.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
+  providers: [UserService],
 })
 export class UserModule {}
