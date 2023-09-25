ffe9585ecd18daf27262b4ab9f3ecefad6fe13d2
Jefferson Barreto <jeffinho_fb@hotmail.com>
2023-09-25 15:12:06 -0300

    feat: insert migration in project

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,10 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user/interfaces/user.entity';
-
+import { CreateTableUser1695655686782 } from './migration/1695655686782-create_table_user';
+import { CreateTableState1695657484025 } from './migration/1695657484025-create_table_state';
+import { CreateTableCity1695657500555 } from './migration/1695657500555-create_table_city';
+import { CreateTableAddress1695657508862 } from './migration/1695657508862-create_table_address';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -16,8 +19,14 @@ import { UserEntity } from './user/interfaces/user.entity';
       password: process.env.DB_PASSWORD,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
-      synchronize: true,
       entities: [UserEntity],
+      migrations: [
+        CreateTableUser1695655686782,
+        CreateTableState1695657484025,
+        CreateTableCity1695657500555,
+        CreateTableAddress1695657508862,
+      ],
+      migrationsRun: true,
     }),
     UserModule,
   ],
